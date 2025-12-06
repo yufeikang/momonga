9eebe2895307578906992ad698f4c97dbfe150ce
NBTK <nbtk@users.noreply.github.com>
2025-12-07 06:44:42 +0900

    Update async_example.py to run concurrent loops for power and energy

--- tests/async_example.py
+++ tests/async_example.py
@@ -9,10 +9,19 @@ async def main():
     dev = os.environ.get('MOMONGA_DEV_PATH')
 
     async with AsyncMomonga(rbid, pwd, dev) as amo:
-        while True:
-            res = await amo.get_instantaneous_power()
-            print('%0.1fW' % res)
-            await asyncio.sleep(60)
+        async def loop_power():
+            while True:
+                res = await amo.get_instantaneous_power()
+                print('Power: %0.1fW' % res)
+                await asyncio.sleep(20)
+
+        async def loop_energy():
+            while True:
+                res = await amo.get_measured_cumulative_energy()
+                print('Energy: %0.1fkWh' % res)
+                await asyncio.sleep(60)
+
+        await asyncio.gather(loop_power(), loop_energy())
 
 
 if __name__ == '__main__':
